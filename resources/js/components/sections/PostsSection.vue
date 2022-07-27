b061d6167bf80d9868f21563933fa9c9d570c29b
AlessP93 <alespiscino@libero.it>
2022-07-27 16:24:31 +0200

    inizio esercizio

---
+++ resources/js/components/sections/PostsSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <div class="container">
+        <ul>
+            <li v-for="post in posts" :key="post.slug">
+                {{post.title}}
+            </li>
+        </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+    name: 'PostsSection',
+    data() {
+        return {
+            posts: []
+        };
+    },
+    created() {
+        axios.get('/api/posts')
+        .then((response) => {
+            this.posts = response.data;
+        })
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
